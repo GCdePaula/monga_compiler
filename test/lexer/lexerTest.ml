e3e44cd6a181fd8ee1c5ae9f32e9cb16eff08108
Gabriel Coutinho <38709327+GCdePaula@users.noreply.github.com>
2019-09-06 13:22:11 -0300

    Fixed opam install. Better tests layout.

--- test/lexer/lexerTest.ml
+++ test/lexer/lexerTest.ml
@@ -1,30 +1,31 @@
 open Src
-
+open Printf
 
 let rec parse lexbuf =
-  let token = Lexer.monga_lexer lexbuf in
-  let c_num = lexbuf.lex_start_p.pos_cnum - lexbuf.lex_start_p.pos_bol + 1 in
-  Printf.printf "%d:%d -- " lexbuf.lex_curr_p.pos_lnum c_num;
-  Types.string_of_tk token;
-  Printf.printf "\n";
+  try
+    let token = Lexer.monga_lexer lexbuf in
+    let c_num = lexbuf.lex_start_p.pos_cnum - lexbuf.lex_start_p.pos_bol + 1 in
+    printf "%d:%d -- " lexbuf.lex_curr_p.pos_lnum c_num;
+    Types.print_tk token;
+    printf "\n";
 
-  match token with
-    | Types.Eof -> ()
-    | _ -> parse lexbuf
-
+    match token with
+      | Types.Eof -> ()
+      | _ -> parse lexbuf
+  with
+    Lexer.LexerError e -> printf "LexerError: %s\n" e;;
 
 let test fileName =
+  printf "LEXING FILE %s\n\n" fileName;
   let cin = open_in fileName in
   let lexbuf = Lexing.from_channel cin in
-  parse lexbuf
-
+  parse lexbuf;
+  printf "\nDONE! %s\n\n" fileName;;
 
 let main () =
-  let fileName = "inputs/test1.in" in
-  LexerTest.test fileName;
-  let fileName = "inputs/test2.in" in
-  LexerTest.test fileName
-
+  let d = "inputs/" in
+  let fnames = [d^"test1.in"; d^"test2.in"; d^"test3.in"] in
+  List.map test fnames;;
 
 let _ = Printexc.print main ()
 
